ee9b046bc25cd62df63d356cc7dd25c012d56f12
My Full Name 1 <bogorodea.bogdancalin@gmail.com>
2023-09-04 06:03:28 +0300

    Upload

---
+++ mysite/ads/owner.py
@@ -0,0 +1,27 @@
+from django.views.generic import DeleteView, ListView, DetailView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class OwnerListView(ListView):
+    """
+    Sub-class the ListView to pass the request to the form.
+    """
+
+
+class OwnerDetailView(DetailView):
+    """
+    Sub-class the DetailView to pass the request to the form.
+    """
+
+
+class OwnerDeleteView(LoginRequiredMixin, DeleteView):
+    """
+    Sub-class the DeleteView to restrict a User from deleting other
+    user's data.
+    """
+
+    def get_queryset(self):
+        print('delete get_queryset called')
+        qs = super(OwnerDeleteView, self).get_queryset()
+        return qs.filter(owner=self.request.user)
